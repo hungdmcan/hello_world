8db21f897ccac152e110895a7da6b96412d14319
daomanhhung <daomanhhung@daos-macbook-pro.local>
2019-05-14 13:31:02 -0400

    Comment addeeeed!!!

--- JavaApplication95/src/javaapplication95/JavaApplication95.java
+++ JavaApplication95/src/javaapplication95/JavaApplication95.java
@@ -4,13 +4,13 @@ package javaapplication95;
  *
  * @author Rich Smith at ZenOfProgramming.com
  */
-public class JavaApplication95 
+public class JavaApplication95
 {
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-        
-    }
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+      int i = 0;
+   }
 }
